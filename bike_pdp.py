74f68d5037a54b6ba4044465d18b2dbcf48f4eaf
jagaiimo <126326547+jagaiimo@users.noreply.github.com>
2025-05-05 10:47:16 +0200

    Update bike_pdp.py

--- bike_pdp.py
+++ bike_pdp.py
@@ -1,20 +1,16 @@
-# bike_pdp.py
-
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.inspection import PartialDependenceDisplay
 
-# Cargar datos
-df = pd.read_csv("day.csv")
 
+df = pd.read_csv("day.csv")
 features = ['instant', 'temp', 'hum', 'windspeed']
 target = 'cnt'
 
 X = df[features]
 y = df[target]
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 model = RandomForestRegressor(n_estimators=100, random_state=42)
